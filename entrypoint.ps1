8fd6190096be4f0df3f5738467536a21435ebddb
Woody1193 <ryanwood1193@gmail.com>
2023-03-07 11:14:51 +0900

    Created PowerShell script and associated action YAML file

---
+++ entrypoint.ps1
@@ -0,0 +1,62 @@
+param (
+    [Parameter(Mandatory)]
+    [string] $Files,
+
+    [Parameter(Mandatory)]
+    [string] $LogFile,
+
+    [Parameter(Mandatory)]
+    [string] $Version,
+
+    [Parameter()]
+    [string[]] $Includes,
+
+    [Parameter()]
+    [bool] $Verbose,
+
+    [Parameter()]
+    [bool] $SyntaxOnly
+)
+
+# Create the download URL for MetaEditor
+$url = "http://www.rosasurfer.com/.mt4/{0}/metaeditor.exe" -f $Version
+$exePath = "metaeditor.exe"
+if ($Verbose) {
+    Write-Host "Attempting download from $($url)..."
+}
+
+# Attempt to download the metaeditor instance from our URL; if this fails then log the error
+try {
+    Invoke-WebRequest -Uri $url -OutFile $exePath
+} catch {
+    if ($Verbose) {
+        Write-Error "Failed to download MetaEditor from $($url), error: $($_.Exception.Message)"
+    }
+}
+
+# Create our new list of arguments and add the portable flag to it
+$args = New-Object System.Collections.Generic.List[string]
+$args.Add("/portable")
+
+# Add the files we should compile to our list of arguments
+$args.Add('/compile:"{0}"' -f $Files)
+
+# Iterate over all the includes we have and convert each to an argument
+foreach ($include in $Includes) {
+    $args.Add('/inc:"{0}"' -f $include)
+}
+
+# Add the log file to our list of arguments
+$args.Add('/log:"{0}"' -f $LogFile)
+
+# If we want to check syntax only then add that flag to the arguments as well
+if ($SyntaxOnly) {
+    $args.Add("/s")
+}
+
+if ($Verbose) {
+    Write-Host "Calling $($exePath) $($args -join " ")..."
+}
+
+# Finally, run metaeditor with our list of arguments
+Start-Process $exePath -ArgumentList $args
