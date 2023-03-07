2fbcdf5a71423f06e8f6f9551ea5d7a2d4d67bf6
Woody1193 <ryanwood1193@gmail.com>
2023-03-07 15:34:27 +0900

    Added debugging

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -34,10 +34,12 @@ try {
     }
 }
 
+Resolve-Path $Files
+
 # Create our new list of arguments and add the portable flag to it
 $args = New-Object System.Collections.Generic.List[string]
 $args.Add("/portable")
-$args.Add('/compile:"{0}"' -f Resolve-Path $Files)
+$args.Add('/compile:"{0}"' -f $Files)
 $args.Add('/inc:"{0}"' -f $Includes)
 $args.Add('/log:"{0}"' -f $LogFile)
 
